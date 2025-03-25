eb786b898fe92bf3ae35f4493db5ce2e1daca731
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-03-25 20:15:23 +0300

    Refactor greeting page

--- server/assets/js/index.js
+++ server/assets/js/index.js
@@ -1,3 +1,6 @@
 import "../scss/index.scss";
 
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../../node_modules/jquery/dist/jquery.slim.js";
+import "../../node_modules/gsap/dist/gsap.js";
+import "./animation/logoStartPage.js";
--- server/router/common.js
+++ server/router/common.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { expressOptions } from "../env.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   res.status(200);
   res.setHeader("Content-Type", "text/html");
-  res.render("startPage/index.html", {});
+  res.render("startPage/main.html", { host: expressOptions.host, port: expressOptions.port });
 });
 
 export default router;
