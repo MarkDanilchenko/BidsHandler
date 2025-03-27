8d260ff8c502fe0301621e0e06969b59a11ad124
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-03-27 22:22:53 +0300

    Configure jest

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -23,7 +23,12 @@ export default {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
